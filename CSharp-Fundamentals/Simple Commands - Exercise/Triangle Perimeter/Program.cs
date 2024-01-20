9ce4eb8799585ff368cc32cce0eaeeeef12eb7b2
assyav <143791358+assyav@users.noreply.github.com>
2024-01-20 17:19:38 +0200

    Added Simple Commands - Exercise

---
+++ CSharp-Fundamentals/Simple Commands - Exercise/Triangle Perimeter/Program.cs
@@ -0,0 +1,6 @@
+﻿int sideA = int.Parse(Console.ReadLine());
+int sideB = int.Parse(Console.ReadLine());  
+int sideC = int.Parse(Console.ReadLine()); 
+
+int perimeter = sideA + sideB + sideC;
+Console.WriteLine(perimeter);
